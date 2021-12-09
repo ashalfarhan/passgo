a75a42a483d9f27219e24a05b754821c3d5f0235
ashalfarhan <ashalfarhan8@gmail.com>
2021-12-09 14:00:11 +0700

    shorthand test

--- test/password_test.go
+++ test/password_test.go
@@ -9,23 +9,20 @@ import (
 
 func TestGeneratorValidation(t *testing.T) {
 	t.Run("should return an error if length is greater than 25", func(t *testing.T) {
-		_, err := password.Generate(1e2, false, false)
-		if err == nil {
+		if _, err := password.Generate(20000000, false, false); err == nil {
 			t.Fatalf("\nexpected error, but got: %v", err)
 		}
 	})
 
 	t.Run("should return an error if length is less than 8", func(t *testing.T) {
-		_, err := password.Generate(-200, false, false)
-		if err == nil {
+		if  _, err := password.Generate(-200, false, false); err == nil {
 			t.Fatalf("\nexpected error, but got: %v", err)
 		}
 	})
 
 	t.Run("should be success generate given length", func(t *testing.T) {
 		ex := 12
-		p, _ := password.Generate(ex, false, false)
-		if ex != len(p.Value) {
+		if p, _ := password.Generate(ex, false, false); ex != len(p.Value) {
 			t.Fatalf("\nexpected result's length to be %v, but got: %v", ex, len(p.Value))
 		}
 	})
